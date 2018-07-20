2f247468979bb80b8431bc6aa99092ea9b9f3e05
Zafar Iqbal <zafar.amt@gmail.com>
2018-07-19 22:46:40 -0400

    Kernel Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Tue Apr 3 21:43:22 EDT 2018"
+#define LINUX_COMPILE_BY "user"
+#define LINUX_COMPILE_HOST "MitySOM-Dev"
+#define LINUX_COMPILER "gcc version 4.7.3 (Ubuntu/Linaro 4.7.3-12ubuntu1) "
